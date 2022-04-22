06bb3d1f63ee20292bdb6afe7422eb5ee1bc875b
Julian Stobbe <jstobbe@physnet.uni-hamburg.de>
2022-04-22 16:47:29 +0200

    minor hlrn changes

--- create_sysimage.jl
+++ create_sysimage.jl
@@ -2,5 +2,12 @@ using PackageCompiler
 using Pkg
 Pkg.activate(@__DIR__)
 
+run_test_path = ARGS[1]
 PackageCompiler.create_sysimage(["LadderDGA"]; sysimage_path="LadderDGA_Precompile.so",
-                                       precompile_execution_file="run_test.jl")
+                                precompile_execution_file=run_test_path,
+                                incremental=true)
+
+PackageCompiler.create_sysimage(["LadderDGA"]; sysimage_path="LadderDGA_Precompile.so",
+                                precompile_execution_file=run_test_path,
+                                incremental=true,
+                                sysimage_build_args=`-O3 --check-bounds=no`)
--- run_test.jl
+++ run_test.jl
@@ -10,31 +10,33 @@ using Distributed
 using LadderDGA
 @everywhere using LadderDGA
 
-cfg_file = "/home/julian/Hamburg/ED_data/asympt_tests/config_14_small.toml"
-@timeit LadderDGA.to "input" mP, sP, env, kGridsStr = readConfig(cfg_file);
-#TODO: loop over kGrids
-@timeit LadderDGA.to "setup" Σ_ladderLoc, Σ_loc, imp_density, kG, gLoc_fft, gLoc_rfft, Γsp, Γch, χDMFTsp, χDMFTch, locQ_sp, locQ_ch, χ₀Loc, gImp = setup_LDGA(kGridsStr[1], mP, sP, env);
+cfg_file_dir = "/scratch/projects/hhp00048/codes/TestRuns_LadderDGA.jl"
+cfg_list = filter(f->endswith(f,"toml"), readdir(cfg_file_dir))
+for cfg_file in cfg_list
+    @timeit LadderDGA.to "input" mP, sP, env, kGridsStr = readConfig(cfg_file);
+    @timeit LadderDGA.to "setup" Σ_ladderLoc, Σ_loc, imp_density, kG, gLoc_fft, gLoc_rfft, Γsp, Γch, χDMFTsp, χDMFTch, locQ_sp, locQ_ch, χ₀Loc, gImp = setup_LDGA(kGridsStr[1], mP, sP, env);
 
-# ladder quantities
-@info "bubble"
-@timeit LadderDGA.to "nl bblt par" bubble_par = calc_bubble_par(gLoc_fft, gLoc_rfft, kG, mP, sP);
-@timeit LadderDGA.to "nl bblt" bubble = calc_bubble(gLoc_fft, gLoc_rfft, kG, mP, sP);
+    # ladder quantities
+    @info "bubble"
+    @timeit LadderDGA.to "nl bblt par" bubble_par = calc_bubble_par(gLoc_fft, gLoc_rfft, kG, mP, sP);
+    @timeit LadderDGA.to "nl bblt" bubble = calc_bubble(gLoc_fft, gLoc_rfft, kG, mP, sP);
 
-@timeit LadderDGA.to "λ₀" begin
-    Fsp = F_from_χ(χDMFTsp, gImp[1,:], sP, mP.β);
-    λ₀ = calc_λ0(bubble, Fsp, locQ_sp, mP, sP)
+    @timeit LadderDGA.to "λ₀" begin
+        Fsp = F_from_χ(χDMFTsp, gImp[1,:], sP, mP.β);
+        λ₀ = calc_λ0(bubble, Fsp, locQ_sp, mP, sP)
+    end
+
+    @info "chi"
+    @timeit LadderDGA.to "nl xsp" nlQ_sp = calc_χγ(:sp, Γsp, bubble, kG, mP, sP);
+    @timeit LadderDGA.to "nl xsp par" nlQ_sp_par = LadderDGA.calc_χγ_par(:sp, Γsp, bubble, kG, mP, sP);
+    @info "parallel data agrees with sequential: " all(nlQ_sp_par.γ .≈ nlQ_sp.γ)
+    @timeit LadderDGA.to "nl xch" nlQ_ch = calc_χγ(:ch, Γch, bubble, kG, mP, sP);
+
+    λsp_old = λ_correction(:sp, imp_density, nlQ_sp, nlQ_ch, gLoc_rfft, λ₀, kG, mP, sP)
+    λsp_new = λ_correction(:sp_ch, imp_density, nlQ_sp, nlQ_ch, gLoc_rfft, λ₀, kG, mP, sP)
+
+    @info "Σ"
+    @timeit LadderDGA.to "nl Σ" Σ_ladder = calc_Σ(nlQ_sp, nlQ_ch, λ₀, gLoc_rfft, kG, mP, sP);
+    #Σ_ladder = Σ_loc_correction(Σ_ladder, Σ_ladderLoc, Σ_loc);
 end
-
-@info "chi"
-@timeit LadderDGA.to "nl xsp" nlQ_sp = calc_χγ(:sp, Γsp, bubble, kG, mP, sP);
-@timeit LadderDGA.to "nl xsp par" nlQ_sp_par = LadderDGA.calc_χγ_par(:sp, Γsp, bubble, kG, mP, sP);
-@info "parallel data agrees with sequential: " all(nlQ_sp_par.γ .≈ nlQ_sp.γ)
-@timeit LadderDGA.to "nl xch" nlQ_ch = calc_χγ(:ch, Γch, bubble, kG, mP, sP);
-
-λsp_old = λ_correction(:sp, imp_density, nlQ_sp, nlQ_ch, gLoc_rfft, λ₀, kG, mP, sP)
-λsp_new = λ_correction(:sp_ch, imp_density, nlQ_sp, nlQ_ch, gLoc_rfft, λ₀, kG, mP, sP)
-
-@info "Σ"
-@timeit LadderDGA.to "nl Σ" Σ_ladder = calc_Σ(nlQ_sp, nlQ_ch, λ₀, gLoc_rfft, kG, mP, sP);
-#Σ_ladder = Σ_loc_correction(Σ_ladder, Σ_ladderLoc, Σ_loc);
 true
