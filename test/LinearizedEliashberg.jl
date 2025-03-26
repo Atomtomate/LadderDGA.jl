900924600db92f42a6c171a942cbb1cb3992b69e
Julian <11405148+Atomtomate@users.noreply.github.com>
2025-03-26 12:08:36 +0100

    Changes Towards working LinEl.Eq
     - default tiall-correctio nscheme set to ExpStep
      - Notebook changed, minor changes in source code

--- test/LinearizedEliashberg.jl
+++ test/LinearizedEliashberg.jl
@@ -8,7 +8,6 @@ end
 function GG_naive(kG, Σ, kp, νp, μ, β)
     Gk = getG_naive(kG, Σ, kp, νp, μ, β)
     G_minusk = getG_naive(kG, Σ, -1 .* kp, -νp-1, μ, β)
-    #println(Gk, " ,", G_minusk)
     return Gk*G_minusk
 end
 
@@ -43,9 +42,10 @@ end
     #println("number of fails in q-lookup: ", length(fails))
     @test length(fails) == 0
     #
+    Gνk_Gmνmk = build_GG(kG, gLoc_i, νnGrid, k_vecs[:])
 
     test_arr = Array{Bool}(undef, length(νnGrid), length(k_vecs))
-
+    test_arr2 = Array{Bool}(undef, length(νnGrid), length(k_vecs))
     for (νpi,νpn) in enumerate(νnGrid)
         GF_νp = LadderDGA.expandKArr(kG, gLoc[:,νpn].parent)
         GF_νp_minus_k_pre = LadderDGA.expandKArr(kG, gLoc[:,-νpn-1].parent)
@@ -55,10 +55,36 @@ end
         for (kpi,kp_vec) in enumerate(k_vecs)
             G_mG  = GG_naive(kG, Σ_loc, kp_vec, νpn, μ, β)
             G_mG2 = GF_νp[kpi]*GF_νp_minus_k[kpi]
-            test_arr[νpi, kpi]  = G_mG ≈ G_mG2
+            test_arr[νpi, kpi]  = (G_mG ≈ G_mG2)
+            test_arr2[νpi, kpi]  = (G_mG ≈ Gνk_Gmνmk[kpi, νpi])
         end
     end
 
     #println("number of fails in G(k)G(-k) precomputation: ", count(.! test_arr))
-    @test count(.! test_arr) == 0
+    @test all(test_arr)
+    @test all(test_arr2)
+end
+
+@testset "DispersionTest" begin
+    kG = LadderDGA.gen_kGrid("2Dsc-1.1-1.2-0.9",8)
+    k_vecs = collect(LadderDGA.Dispersions.gen_sampling(LadderDGA.Dispersions.grid_type(kG), LadderDGA.Dispersions.grid_dimension(kG), kG.Ns))
+    
+    dispersion_tmp(kvec, t, tp, tpp) = -2 * t * sum(cos.(kvec)) +  4 * tp  * cos(kvec[1])*cos(kvec[2]) - 2 * tpp * sum(cos.(2 .* kvec))
+    #checks = falses(length(k_vecs), length(k_vecs))
+    #q_vecs = Array{Tuple{Float64, Float64}}(undef, length(k_vecs), length(k_vecs))
+    #k_minus_kp = Array{Tuple{Float64, Float64}}(undef, length(k_vecs), length(k_vecs))
+    #k_minus_kp_mapped = Array{Tuple{Float64, Float64}}(undef, length(k_vecs), length(k_vecs))
+    ϵkGrid_test = Array{Float64}(undef, length(k_vecs), length(k_vecs))
+    ϵkGrid_exact = Array{Float64}(undef, length(k_vecs), length(k_vecs))
+
+    for (i,ki) in enumerate(k_vecs)
+        for (j,kpi) in enumerate(k_vecs)
+            #q_vecs[i,j] = kG.kGrid[qi_access[i,j]]
+            #k_minus_kp[i,j] = ki .- kpi
+            #k_minus_kp_mapped[i,j] = LadderDGA.Dispersions.transform_to_first_BZ(kG, ki .- kpi)
+            ϵkGrid_test[i,j] = kG.ϵkGrid[qi_access[i,j]]
+            ϵkGrid_exact[i,j] = dispersion_tmp(ki .- kpi, kG.t, kG.tp, kG.tpp)
+        end
+    end
+    @test all(ϵkGrid_test .≈ ϵkGrid_exact)
 end
